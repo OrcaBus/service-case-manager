fa8b6e289107254a4931788952feb924e1fd8260
william <william.intan@unimelb.edu.au>
2026-05-15 21:19:57 +1000

    Update index.ts

--- infrastructure/stage/construct/lambda-metadata-linking/index.ts
+++ infrastructure/stage/construct/lambda-metadata-linking/index.ts
@@ -6,7 +6,9 @@ import { formatRdsPolicyName } from '@orcabus/platform-cdk-constructs/shared-con
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
 import { EVENT_BUS_NAME } from '@orcabus/platform-cdk-constructs/shared-config/event-bridge';
-
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { JWT_SECRET_NAME } from '@orcabus/platform-cdk-constructs/shared-config/secrets';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 export const REDCAP_TOKEN_PARAMETER_NAME = '/orcabus/case-manager/redcap/redcap-api-token';
 
 type RedCapLambdaProps = {
@@ -40,10 +42,27 @@ export class LambdaMetadataEntityLinkConstruct extends Construct {
       )
     );
 
+    // pass the domain name for other services
+    const hostedZoneName = StringParameter.valueFromLookup(this, '/hosted_zone/umccr/name');
+    this.lambda.addEnvironment('HOSTED_ZONE_NAME', hostedZoneName);
+
+    // allow lambda to retrieve the service user JWT
+    const serviceUserJwtSecret = Secret.fromSecretNameV2(
+      this,
+      'serviceUserJwtSecret',
+      JWT_SECRET_NAME
+    );
+    this.lambda.addEnvironment('ORCABUS_SERVICE_JWT_SECRET_ARN', serviceUserJwtSecret.secretArn);
+    serviceUserJwtSecret.grantRead(this.lambda);
+
+    const orcabusEventBus = EventBus.fromEventBusName(this, 'EventBus', EVENT_BUS_NAME);
+    orcabusEventBus.grantPutEventsTo(this.lambda);
+    this.lambda.addEnvironment('EVENT_BUS_NAME', EVENT_BUS_NAME);
+
     // Add EventBridge rule to trigger Lambda on MetadataStateChange events from orcabus.metadatamanager
     const redCapLambdaEventTarget = new LambdaFunction(this.lambda);
     new Rule(this, 'RedCapLambdaTriggerOnMetadataStateChange', {
-      eventBus: EventBus.fromEventBusName(this, 'OrcaBusEventBus', EVENT_BUS_NAME),
+      eventBus: orcabusEventBus,
       description:
         'Rule to trigger Lambda on MetadataStateChange events from orcabus.metadatamanager',
       eventPattern: {
